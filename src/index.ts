1b4f24be5390b605aa844b9287c4093a7656b656
raymond.vanvelse@netprogroup.com <raymond.vanvelse@netprogroup.com>
2024-12-21 20:15:12 +0100

    fixes

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,6 @@
 export type { PrayerTimesProps } from "../types/prayer-times";
 export { PrayerTimes } from "./components/prayer-times";
 
-// Shadcn UI Components used in PrayerTimes
-export { Alert, AlertDescription } from "./components/ui/alert";
-export { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card";
-export { Skeleton } from "./components/ui/skeleton";
-
 // Types
 export type { AladhanResponse, PrayerTime } from "../types/prayer-times";
 
